4fa9eceb5a87bea4ba391fd08685e2e7cad4005a
Mai Bảo Long <66893806+zitsieunhan1512@users.noreply.github.com>
2023-02-24 15:43:12 +0700

    first commit!

---
+++ app/src/main/java/com/mblhcmute/implicitintenttest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.mblhcmute.implicitintenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import com.mblhcmute.implicitintenttest.databinding.ActivityMainBinding;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    public void launchWebPageButtonClicked(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://fhqx.hcmute.edu.vn"));
+        startActivity(intent);
+    }
+}
